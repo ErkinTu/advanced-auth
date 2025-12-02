2e42effc35c346bf62a0ad3d6e7bff765d63172b
Erkin <erkint@gmail.com>
2025-12-02 21:33:34 +0600

    🎉 Initial frontend

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    Header
+  </header>
+
+  <main>
+    <div class="text-2xl">Main</div>
+  </main>
+
+  <footer>
+    Footer
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
