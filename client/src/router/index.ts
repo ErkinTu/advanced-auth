600b9095838a34f31d4b6c5f5b5f37612812a3ac
Erkin <erkint@gmail.com>
2025-12-06 00:14:24 +0600

    ♻️ Refactor Vue Router structure to group routes under shared `Layout` component and add `beforeEach` navigation guard for `Users` route

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,44 +1,60 @@
 import {createRouter, createWebHistory} from 'vue-router';
+import {useUserStore} from "@/store/auth.ts";
 
+
+import Layout from '@/layouts/Layout.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-    meta: {public: true}
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('../views/Home.vue'),
+        meta: { public: true }
+      },
+      {
+        path: 'login',
+        name: 'Login',
+        component: () => import('../views/Login.vue'),
+        meta: { public: true }
+      },
+      {
+        path: 'register',
+        name: 'Register',
+        component: () => import('../views/Register.vue'),
+        meta: { public: true }
+      },
+      {
+        path: 'users',
+        name: 'Users',
+        component: () => import('../views/Users.vue'),
+        meta: { public: true }
+      },
+    ]
   },
-  {
-    path: '/register',
-    name: 'Register',
-    component: () => import('../views/Register.vue'),
-    meta: {public: true} // + guestOnly: true
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue'),
-    meta: {public: true} // + guestOnly: true
-  },
-  {
-    path: '/activate/:token',
-    name: 'Activation',
-    component: () => import('../views/Activation.vue'),
-    meta: {public: true}
-  },
-  {
-    path: '/users',
-    name: 'Users',
-    component: () => import('../views/Users.vue'),
-    meta: {public: true} // adminOnly: true
-  },
+
+  // 404 fallback
   {
     path: '/:pathMatch(.*)*',
     redirect: '/'
   }
-];
+]
+
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach(async (to, from, next) => {
+  if (to.name === 'Users') {
+    const store = useUserStore()
+    if (store.users.length === 0) {
+      await store.loadUsers()
+    }
+  }
+  next()
+})
