223fcca45acdfeb032857b573749615033c882ba
Erkin <erkint@gmail.com>
2025-12-08 21:52:15 +0600

    ✨ Add `Profile` view with user details using reusable `TableContainer` component

---
+++ client/src/views/Profile.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import TableContainer from "@/components/ui/TableContainer.vue";
+
+const user = ref({
+  id: 1,
+  email: 'erkin@gmail.com',
+  is_activated: true,
+  role: 'admin'
+})
+</script>
+
+<template>
+  <div class="p-6">
+    <TableContainer
+      :title="user.email"
+      description="User information and activation status."
+    >
+      <p>Role <strong>{{user.role}}</strong></p>
+      <p>Status <strong>{{user.is_activated ? 'activated' : 'no activated'}}</strong></p>
+    </TableContainer>
+  </div>
+</template>
