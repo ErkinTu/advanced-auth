07806b0d084799e28b7a14f8fdc5ff9cdaf1e2ee
Erkin <erkint@gmail.com>
2025-12-08 21:53:03 +0600

    ✨ Add `Profile` link to `AppNav` navigation

--- client/src/components/layout/AppNav.vue
+++ client/src/components/layout/AppNav.vue
@@ -5,5 +5,6 @@
   <div class="flex gap-2 text-sm">
     <router-link to="/">Home</router-link>
     <router-link to="/users">Users</router-link>
+    <router-link to="/profile">Profile</router-link>
   </div>
 </template>
