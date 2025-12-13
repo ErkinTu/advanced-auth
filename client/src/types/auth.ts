bc23adee78fce0022a20c73616d83e531dc4636b
Erkin <erkint@gmail.com>
2025-12-14 00:20:14 +0600

    ✨ Extend `User` type with optional `roles` property and add `Role` interface to improve type definitions in `auth` module

--- client/src/types/auth.ts
+++ client/src/types/auth.ts
@@ -1,25 +1,23 @@
+export interface Role {
+  id: number
+  name: string
+}
+
 export interface User {
   id: number
   email: string
   is_activated: boolean
   created_at: string
   updated_at: string
+  roles?: Role[]
 }
-
 
 export interface AuthCredentials {
   email: string
   password: string
 }
 
-
 export interface AuthResponse {
   message: string
-  token: string // access token
-}
-
-
-export interface ApiResponse<T> {
-  message: string
-  data: T
+  user?: User
 }
